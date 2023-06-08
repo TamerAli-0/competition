ad980a1361badac099de1e64048382437788ea78
Firebreak-2 <xxibahimgamerxx123123@gmail.com>
2023-06-08 09:34:46 +0400

    Add database readers

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import csv
+import os
 
-print('Hello, World!')
+# Prompt the user to enter the filename
+filename = input("Enter the filename (including the .csv extension): ")
+
+# Check if the file exists
+if not os.path.isfile(filename):
+    # Create an empty file
+    with open(filename, "w") as file:
+        pass
+
+# Open the file and read its contents
+with open(filename, "r") as file:
+    # Create a CSV reader object
+    csv_reader = csv.reader(file)
+
+    # Iterate over each row in the CSV file
+    for row in csv_reader:
+        # Print each entry
+        print("ID: {}, Name: {}, Cost: {}".format(row[0], row[1], row[2]))
